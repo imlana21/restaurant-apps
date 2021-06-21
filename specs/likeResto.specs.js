ed6f133a61ad1495d4a9084e8e81613acc19297a
lana404 <ihwan.cfc@gmail.com>
2021-06-21 17:12:35 +0700

    add: unit-test

--- specs/likeResto.specs.js
+++ specs/likeResto.specs.js
@@ -1,5 +1,3 @@
-
-
 describe('like button resto testing', () => {
   it('should show the like button when the resto has not been liked before', () => {
     document.body.innerHTML = '<div id="like_container"> </div>';
